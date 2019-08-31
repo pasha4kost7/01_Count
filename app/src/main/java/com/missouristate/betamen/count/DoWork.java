8ad9b6d7f4909929132d8da584c2d42ff36046ac
Pavel <pasha@gsdghrte.com>
2019-08-31 00:32:09 -0500

    The count app

---
+++ app/src/main/java/com/missouristate/betamen/count/DoWork.java
@@ -0,0 +1,9 @@
+package com.missouristate.betamen.count;
+
+class DoWork {
+    public static int addNumber(int num){
+        return num=num +1;
+
+    }
+
+}
---
+++ app/src/main/java/com/missouristate/betamen/count/MainActivity.java
@@ -0,0 +1,66 @@
+package com.missouristate.betamen.count;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Button btn = (Button) findViewById(R.id.button_1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.tv_1);
+                String stringVallue =textView.getText().toString();
+                int origNum = Integer.parseInt(stringVallue);
+                int newValue = DoWork.addNumber(origNum);                               // sends newValue to a new class doWork
+                textView.setText(Integer.toString(newValue));
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
